b3947ce82246319dfe91e2d7e4172c9baa7c944b
不明栗子 <94903610+unknownparticles@users.noreply.github.com>
2022-10-31 20:41:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function btn() {
     setTimeout(cb, 100);
     if (document.getElementById('sp').innerHTML % 100 == 0) {
         document.getElementById("lau").play();
-        document.getElementById("img1").src = 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbpic.588ku.com%2Felement_pic%2F00%2F16%2F06%2F085757c3df566ae.jpg&refer=http%3A%2F%2Fbpic.588ku.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1669652351&t=0fe15e5c1ee27b52269a1029dddc7ea6';
         alert("功德数满100圣诞老人陪你一起笑！");
         cb;
     }
